7e36b23f2b771f4deea6d1a420ff5a6d447345a9
Carlos Ariel <ariels7801@gmail.com>
2017-10-07 21:33:52 -0500

    Implementing post method in Web API

--- EmployeeService/Controllers/EmployeesController.cs
+++ EmployeeService/Controllers/EmployeesController.cs
@@ -19,14 +19,52 @@ namespace EmployeeService.Controllers
             } 
         }
 
-        public Employee Get(int id)
+        public HttpResponseMessage Get(int id)
         {
-            using (EmployeeDBEntities entities = new EmployeeDBEntities())
+            try
             {
-                return entities.Employees.FirstOrDefault(e => e.ID == id);
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    var entity = entities.Employees.FirstOrDefault(e => e.ID == id);
+
+                    if (entity != null)
+                    {
+                        return Request.CreateResponse(HttpStatusCode.OK, entity);
+                    }
+                    else
+                    {
+                        return Request.CreateErrorResponse(HttpStatusCode.NotFound,"Employee with Id = " + id.ToString() + " not found");
+                    }
+                    
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
             }
         }
 
+
+        public HttpResponseMessage Post([FromBody]Employee employee)
+        {
+            try
+            {
+                using (EmployeeDBEntities entities = new EmployeeDBEntities())
+                {
+                    entities.Employees.Add(employee);
+                    entities.SaveChanges();
+
+                    var message = Request.CreateResponse(HttpStatusCode.Created, employee);
+                    message.Headers.Location = new Uri(Request.RequestUri + employee.ID.ToString());
+                    return message;
+                }
+            }
+            catch (Exception ex)
+            {
+                return Request.CreateErrorResponse(HttpStatusCode.BadRequest, ex);
+            }
+
+        }
 
     }
 }
