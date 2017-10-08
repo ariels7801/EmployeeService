31d900fd1ec141d94c89a458961613d901642e3f
Carlos Ariel <ariels7801@gmail.com>
2017-10-07 20:17:52 -0500

    Commit Incial

---
+++ EmployeeService/App_Start/WebApiConfig.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Web.Http;
+using Microsoft.Owin.Security.OAuth;
+using Newtonsoft.Json.Serialization;
+using System.Net.Http.Formatting;
+using System.Net.Http.Headers;
+
+namespace EmployeeService
+{
+
+    public class CustomJsonFormatter: JsonMediaTypeFormatter
+    {
+        public CustomJsonFormatter() {
+            this.SupportedMediaTypes.Add(new MediaTypeHeaderValue("text/html"));
+        }
+
+        public override void SetDefaultContentHeaders(Type type, HttpContentHeaders headers, MediaTypeHeaderValue mediaType)
+        {
+            base.SetDefaultContentHeaders(type, headers, mediaType);
+            headers.ContentType = new MediaTypeHeaderValue("application/json");
+        }
+    }
+    public static class WebApiConfig
+    {
+        public static void Register(HttpConfiguration config)
+        {
+            // Web API configuration and services
+            // Configure Web API to use only bearer token authentication.
+            config.SuppressDefaultHostAuthentication();
+            config.Filters.Add(new HostAuthenticationFilter(OAuthDefaults.AuthenticationType));
+
+            // Web API routes
+            config.MapHttpAttributeRoutes();
+
+            config.Routes.MapHttpRoute(
+                name: "DefaultApi",
+                routeTemplate: "api/{controller}/{id}",
+                defaults: new { id = RouteParameter.Optional }
+            );
+
+            //Forma3
+            config.Formatters.Add(new CustomJsonFormatter());
+
+            //Forma2
+            //config.Formatters.Remove(config.Formatters.XmlFormatter);
+
+            //Forma 1
+            //config.formatters.jsonformatter.serializersettings.formatting = newtonsoft.json.formatting.indented;
+            //config.Formatters.JsonFormatter.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.CamelCasePropertyNamesContractResolver();
+        }
+    }
+}
